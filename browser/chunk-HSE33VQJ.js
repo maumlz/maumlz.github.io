3d8d8e6a132088fc0b4430f0461627abf020db53
Mauricio Meléndez <mauricio.mlzg@gmail.com>
2026-06-01 15:33:10 -0600

    Auto-generated commit

---
+++ browser/chunk-HSE33VQJ.js
@@ -0,0 +1,1 @@
+import{Aa as a,Ba as n,Ea as s,qa as o}from"./chunk-LPKAO4E2.js";var i=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-about"]],decls:2,vars:0,template:function(t,m){t&1&&(a(0,"p"),s(1,"about works!"),n())},encapsulation:2})};export{i as About};
