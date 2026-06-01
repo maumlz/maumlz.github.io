8f692e8c3e8cf741f5eb40e7869e5ed792e92ef4
Mauricio Meléndez <mauricio.mlzg@gmail.com>
2026-06-01 15:28:54 -0600

    version 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home)
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('./pages/about/about').then(m => m.About)
+    },
+    {
+        path: 'resume',
+        loadComponent: () => import('./pages/resume/resume').then(m => m.Resume)
+    }
+];
